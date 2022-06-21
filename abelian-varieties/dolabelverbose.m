4fe3c06e35da0d84aebf0c737c8a7d0d241a7f40
Edgar Costa <edgarc@mit.edu>
2022-06-21 02:05:21 +0000

    reorganization to include number field output

--- abelian-varieties/dolabelverbose.m
+++ abelian-varieties/dolabelverbose.m
@@ -1,7 +1,13 @@
 AttachSpec("~/projects/CHIMP/CHIMP.spec");
 AttachSpec("~/projects/ModularCurves/abelian-varieties/spec");
 SetVerbose("ModAbVarRec", 2);
-SetVerbose("ModularSymbols", 2);
+//SetVerbose("ModularSymbols", 2);
+//SetVerbose("RieSrf", 2);
+SetVerbose("CurveRec", 2);
+SetVerbose("EndoFind", 3);
+SetColumns(0);
+SetAutoColumns(false);
+SetDebugOnError(true);
 s := Split(input, ":");
 label := s[1];
 level := StringToInteger(s[2]);
@@ -9,8 +15,10 @@ hc := eval s[3];
 prec := StringToInteger(prec);
 ncoeffs := Ceiling(20*Sqrt(level)*Log(10)*prec/(2*Pi(ComplexField())));
 time f := MakeNewformModSym(level, hc);
+b, out := ReconstructGenus2Curve(f : prec:=prec, ncoeffs:=ncoeffs,  D:=[-20..20], UpperBound:=12);
+/*
 try
-  b, out := ReconstructGenus2Curve(f : prec:=prec, ncoeffs:=ncoeffs);
+  b, out := ReconstructGenus2Curve(f : prec:=prec, ncoeffs:=ncoeffs);//, D:=[-2..2]);
   if not b then
     error "failed to find *any* curves!!";
   end if;
@@ -20,3 +28,4 @@ catch e
 end try;
 StripWhiteSpace(Sprintf("%o:%o", label, MachinePrint(out)));
 exit;
+*/
