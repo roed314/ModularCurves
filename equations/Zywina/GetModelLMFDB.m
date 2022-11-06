2344ced3057d2667bc78f07a88434fd5524b8ee9
Eran Assaf <assaferan@gmail.com>
2022-11-06 17:53:09 -0500

    updated GetModelLMFDB to use Jeremy's code for computing precision and the j-map

--- equations/Zywina/GetModelLMFDB.m
+++ equations/Zywina/GetModelLMFDB.m
@@ -1,5 +1,15 @@
+// Usage: ls input_data | parallel --timeout 600 magma -b label:={1} GetModelLMFDB.m
+// where input_data is a folder containing one file for each label, consisting of the generators as a comma-separated list of integers
+
 AttachSpec("ModCrv.spec");
-import "ModularCurves.m" : CreateModularCurveRec, FindModelOfXG;
+import "findjinvmap.m" : FindJMapInv, GetPrecisionAndDegrees;
+import "ModularCurves.m" : CreateModularCurveRec, FindCanonicalModel;
+
+if (not assigned label) then
+  printf "This script assumes that label, the label of the X_H to compute, is given as a command line paramter.\n";
+  printf "Something like magma label:=7.168.3.1 findjmap.m\n";
+  quit;  
+end if;
 
 System("mkdir -p ../output_data");
 SetColumns(0);
@@ -13,8 +23,27 @@ assert #gens mod 4 eq 0;
 gens := [gens[4*(i-1)+1..4*i] : i in [1..#gens div 4]];
 M := CreateModularCurveRec(level, gens);
 assert M`genus ge 3;
-prec := RequiredPrecision(M);
-X := FindModelOfXG(M, prec);
-E4, E6, j := JMap(X);
-LMFDBWriteModel(X, E4, E6, "../output_data/" * label);
+prec, mind, maxd := GetPrecisionAndDegrees(M);
+// X := FindModelOfXG(M, prec);
+printf "Starting model computation.\n";
+ttemp := Cputime();
+flag, psi, F := FindCanonicalModel(M, prec);
+if flag then
+    M`k := 2;
+    M`F0 := F;
+    M`psi := psi;
+else
+    printf "The modular curve with label %o is geometrically hyperelliptic.\n";
+    quit;
+end if;
+modeltime := Cputime(ttemp);
+printf "Done. Time taken was %o.\n",modeltime;
+printf "Model time = %o.\n",modeltime;
+j := New(JMapData);
+jmap, num, denom, pts := FindJMapInv(M, prec, mind, maxd);
+eis_time := Cputime();
+j`E4, j`E6, _ := JMap(M);
+printf "E4,E6 time taken was %o. \n", eis_time;
+j`J := num / denom;
+LMFDBWriteModel(M, j, "../output_data/" * label);
 exit;
