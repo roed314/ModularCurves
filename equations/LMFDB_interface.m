76288daaf30f59d9eed520033ded6ae12e92df23
Sam Schiavone <sam.schiavone@gmail.com>
2022-11-07 13:55:50 -0500

    make intrinsic

--- equations/LMFDB_interface.m
+++ equations/LMFDB_interface.m
@@ -2,15 +2,18 @@
 // AttachSpec("~/github/CHIMP/CHIMP.spec");
 
 // Expects a line as in modular_curves_data.txt
-function GetModularCurveGeneratorsForLine(line) 
-    cid, label, level, gen_str := Explode(Split(line, "|"));
-    N := StringToInteger(level);
-    gen_str := ReplaceCharacter(gen_str, "{", "[");
-    gen_str := ReplaceCharacter(gen_str, "}", "]");
-    gen_list := eval gen_str;
-    Mat := MatrixRing(Integers(N), 2);
-    return [Mat!elt : elt in gen_list];
-end function;
+//function GetModularCurveGeneratorsForLine(line) 
+intrinsic GetModularCurveGeneratorsForLine(line::MonStgElt) -> SeqEnum
+  {}
+  cid, label, level, gen_str := Explode(Split(line, "|"));
+  N := StringToInteger(level);
+  gen_str := ReplaceCharacter(gen_str, "{", "[");
+  gen_str := ReplaceCharacter(gen_str, "}", "]");
+  gen_list := eval gen_str;
+  Mat := MatrixRing(Integers(N), 2);
+  return [Mat!elt : elt in gen_list];
+end intrinsic;
+//end function;
 
 // Given an LMFDB label for a modular curve, return corresponding generators
 //function GetModularCurveGenerators(label : path_to_data := "modular_curves_data.txt")
