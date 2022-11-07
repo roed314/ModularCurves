59175fa46c7c72d865fea47319c971b2b480c555
Sam Schiavone <sam.schiavone@gmail.com>
2022-11-07 15:33:48 -0500

    trying g4 example

--- equations/examples/9.108.4.1-example.m
+++ equations/examples/9.108.4.1-example.m
@@ -1,18 +1,18 @@
 // running in equations/Zywina
-AttachSpec("ModCrv.spec");
+AttachSpec("equations.spec");
 AttachSpec("~/github/CHIMP/CHIMP.spec");
 AttachSpec("~/github/Gm-Reduce/spec");
 SetVerbose("GmReduce",true);
-load "../LMFDB_interface.m";
+load "OpenImage/main/GL2GroupTheory.m";
+load "OpenImage/main/ModularCurves.m";
+import "Zywina/findjinvmap.m": GetPrecisionAndDegrees, FindJMapInv;
 
 QQ := Rationals();
-ChangeDirectory("~/github/ModularCurves/equations");
 gens := GetModularCurveGenerators("9.108.4.1");
-ChangeDirectory("Zywina");
 N := Characteristic(BaseRing(Parent(gens[1])));
 rec := CreateModularCurveRec(N,gens);
 //bool, polys, fs := FindCanonicalModel(rec,20);
-rec := FindModelOfXG(rec, 20);
+rec := FindModelOfXG(rec, 50);
 /*
   S := Parent(polys[1]);
   S := ChangeRing(S,QQ);
@@ -25,6 +25,8 @@ K := RationalsAsNumberField();
 C := ChangeRing(C,K);
 Ps := [C!Eltseq(el) : el in Ps];
 Ps := [Place(el) : el in Ps];
-KC<x,y,z> := FunctionField(C);
-time small3 := SmallFunctions(Ps,3);
-
+KC<x,y,z> := FunctionField(AffinePatch(C,1));
+//time small3 := SmallFunctions(Ps,3);
+E4, E6, j := JMap(rec);
+jC := RationalFunctionToFunctionFieldElement(C,j);
+BestModel(jC);
