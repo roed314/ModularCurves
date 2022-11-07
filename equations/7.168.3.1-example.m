a41ae705584a720b7799d29354e3e3b5fbe172e5
Sam Schiavone <sam.schiavone@gmail.com>
2022-11-07 10:40:35 -0500

    move examples

--- equations/7.168.3.1-example.m
+++
@@ -1,8 +0,0 @@
-QQ := Rationals();
-gens := GetModularCurveGenerators("7.168.3.1");
-N := Characteristic(BaseRing(Parent(gens[1])));
-rec := CreateModularCurveRec(N,gens);
-bool, polys, fs := FindCanonicalModel(rec,20);
-S := Parent(polys[1]);
-S := ChangeRing(S,QQ);
-C := Curve(Proj(S),polys);
