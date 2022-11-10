91ebca76c60eace1a1a677a99ffe85c13ed4c1a7
Eran Assaf <assaferan@gmail.com>
2022-11-09 20:34:51 -0500

    updating tests

--- equations/test_jmap.sh
+++ equations/test_jmap.sh
@@ -1,5 +1,7 @@
-rm -f output_data/30.30.3.1
-cd Zywina
-magma -b label:=30.30.3.1 GetModelLMFDB.m > ../stdout/30.30.3.1
-cd ..
+for label in 30.30.3.1 6.6.1.1 6.12.1.1 11.55.1.1 8.48.1.3 9.54.1.1 20.72.1.23 8.96.1.109 1.1.0.1 2.2.0.1 3.6.0.2 10.120.5.3 12.144.3.1 7.168.3.1
+do
+    rm -f output_data/$label*
+    rm -f stdout/$label
+    magma -b label:=$label GetModelLMFDB.m > stdout/$label
+done
 magma -b tests/test_30.30.3.1.m
