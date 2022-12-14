c7ac325cd0e7caa40da3bc54a9b8e8f506e8af75
David Roe <roed.math@gmail.com>
2022-12-14 15:04:05 -0500

    Also find labels for descents

--- upload/descends_ecnf.py
+++ upload/descends_ecnf.py
@@ -9,22 +9,47 @@ parser.add_argument("nf", help="number field, base for elliptic curves")
 
 sys.path.append(os.path.expanduser(opj("~", "lmfdb")))
 
-from sage.all import EllipticCurve, NumberField, PolynomialRing, QQ
+from sage.all import EllipticCurve, NumberField, PolynomialRing, ZZ, QQ
 from lmfdb import db
+from collections import defaultdict
 
 args = parser.parse_args()
 
-R = PolynomialRing(QQ, name="x")
+R = PolynomialRing(ZZ, name="x")
 Kdata = db.nf_fields.lookup(args.nf, ["coeffs", "subfields"])
-subfields = [NumberField(R([QQ(c) for c in K.split(".")]), "a") for K in Kdata["subfields"]]
-K0 = NumberField(R(Kdata["coeffs"]), "b")
+coeffs = [[ZZ(c) for c in K.split(".")] for K in Kdata["subfields"]]
+subfields = [NumberField(R(clist), "a") for clist in coeffs]
+subfield_labels = [db.nf_fields.lucky({"coeffs": clist}, "label") for clist in coeffs]
+by_jinvs = []
+for K, Klabel in zip(subfields, subfield_labels):
+    by_jinvs.append(defaultdict(list))
+    for rec in db.ec_nfcurves.search({"field_label": Klabel}, ["label", "jinv", "ainvs"]):
+        j = K([QQ(c) for c in rec["jinv"].split(",")])
+        ainvs = [K([QQ(c) for c in a.split(",")]) for a in rec["ainvs"].split(";")]
+        E = EllipticCurve(ainvs)
+        by_jinvs[-1][j].append((E, rec["label"]))
+L = NumberField(R(Kdata["coeffs"]), "b")
+embss = [K.embeddings(L) for K in subfields]
 with open(opj("ecnf_bc", args.nf), "w") as F:
     if subfields:
-        for rec in db.ec_nfcurves.search({"base_change":[], "field_label": args.nf}, ["label", "ainvs"]):
-            ainvs = [K0([QQ(c) for c in a.split(",")]) for a in rec["ainvs"].split(";")]
+        for rec in db.ec_nfcurves.search({"field_label": args.nf}, ["label", "ainvs"]):
+            ainvs = [L([QQ(c) for c in a.split(",")]) for a in rec["ainvs"].split(";")]
             E = EllipticCurve(ainvs)
-            for K in subfields:
-                Edesc = E.descend_to(K)
-                if Edesc:
-                    _ = F.write(f"{rec['label']}|{K.defining_polynomial()}|{[Ed.ainvs() for Ed in Edesc]}\n")
+            for K, embs, by_jinv in zip(subfields, embss, by_jinvs):
+                Edesc = E.descend_to(K, embs[0])
+                desc_labels = set()
+                i = 0
+                while len(desc_labels) < len(Edesc) and i < len(embs):
+                    if i != 0:
+                        Edesc = E.descend_to(K, embs[i])
+                    i += 1
+                    for EK in Edesc:
+                        j = EK.j_invariant()
+                        if j in by_jinv:
+                            for EK1, label1 in by_jinv[j]:
+                                if EK.is_isomorphic(EK1):
+                                    desc_labels.add(label1)
+                                    break
+                missing = "missing" if (len(desc_labels) < len(Edesc)) else "found"
+                _ = F.write(f"{rec['label']}|{K.defining_polynomial()}|{[Ed.ainvs() for Ed in Edesc]}|{desc_labels}|{missing}\n")
     _ = F.write("Done")
