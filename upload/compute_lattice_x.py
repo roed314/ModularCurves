29b03a10399fa8ff8568470fd4be05fee0500397
David Roe <roed.math@gmail.com>
2022-12-20 04:52:43 -0500

    Fix fstring

--- upload/compute_lattice_x.py
+++ upload/compute_lattice_x.py
@@ -788,8 +788,9 @@ def create_db_uploads():
 
     with open("gps_gl2zhat_fine.update", "w") as F:
         _ = F.write("label|q_gonality|qbar_gonality|q_gonality_bounds|qbar_gonality_bounds|lattice_labels|lattice_x\ninteger|integer|integer[]|integer[]|text[]|integer[]\n\n")
+        default = r"\N|\N"
         for label, gon in gonalities.items():
-            _ = F.write(f"{transform_label(label)}|{gon}|{data['L'][label][0]}\n")
+            _ = F.write(f"{transform_label(label)}|{gon}|{data['L'].get(label, [default])[0]}\n")
 
     # Construct modcurve_points
     nf_lookup = {rec["coeffs"]: rec["label"] for rec in db.nf_fields.search({"degree":{"$lte":6}}, ["label", "coeffs"])}
