563850eab07d271ee0e128b27b0e66dbc5786bd6
David Roe <roed.math@gmail.com>
2022-12-21 18:59:14 -0500

    Fix typo

--- equations/clean_testrun.py
+++ equations/clean_testrun.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser("Clean up from a test run of cloud_start.py")
 parser.add_argument("saven", help="number where output should be moved")
 parser.add_argument("savecan", action="store_true", help="whether to keep the canonical_models folder")
 
-args = parser.parser_args()
+args = parser.parse_args()
 
 prefix = "mid" + args.saven + "_"
 tomove = ["ghyp_models", "graphviz_out", "output", "plane_models", "rats", "stdout", "timings"]
