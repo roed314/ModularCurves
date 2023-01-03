a831403f4293936dc87452e3418e7eee92847579
David Roe <roed.math@gmail.com>
2023-01-03 17:04:44 -0500

    Bash expansion

--- upload/make_pictures.py
+++ upload/make_pictures.py
@@ -10,4 +10,7 @@ args = parser.parse_args()
 
 os.makedirs("picture_stdout", exist_ok=True)
 n = args.num_jobs
-subprocess.run('parallel -j%s "sage make_modular_curve_picture.sage {1} %s >> picture_stdout/{1}" ::: {0..%s}' % (n, n, n-1), shell=True)
+# Unfortunately we don't get bash expansion so can't use {0..n-1}
+# n should be small enough that we can just do it inline
+assert n < 500
+subprocess.run('parallel -j%s "sage make_modular_curve_picture.sage {1} %s >> picture_stdout/{1}" ::: %s' % (n, n, " ".join(str(c) for c in range(n))), shell=True)
